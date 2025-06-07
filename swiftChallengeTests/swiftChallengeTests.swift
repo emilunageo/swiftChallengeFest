ec87438eb70629d77a4e6fd0d46972f29c0b2f57
Emiliano Luna George <48260618+emilunageo@users.noreply.github.com>
2025-06-07 13:01:16 -0600

    Initial Commit

---
+++ swiftChallengeTests/swiftChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftChallengeTests.swift
+//  swiftChallengeTests
+//
+//  Created by Emiliano Luna on 07/06/25.
+//
+
+import Testing
+@testable import swiftChallenge
+
+struct swiftChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
